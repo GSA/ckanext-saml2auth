d7857a683616da0ff5c051c2d9c5207358b50c0d
Tyler Burton <tyler.burton@gsa.gov>
2025-01-02 11:38:00 -0600

    remove add_resource fanstatic

--- ckanext/saml2auth/plugin.py
+++ ckanext/saml2auth/plugin.py
@@ -91,7 +91,7 @@ class Saml2AuthPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'saml2auth')
+        # toolkit.add_resource('fanstatic', 'saml2auth')
 
     # IAuthenticator
 
